2064cd930994a54dc4d85b63ee2b6a89e8fa9966
MSAkain <theggodisthere@gmail.com>
2022-04-25 12:07:10 +0300

    added new sections

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,11 @@
-$(function () {
+const swiper = new Swiper('.gallery__swiper', {
 
-
-	$('.reviews__slider').slick({
-		fade: true,
-		infinite: false,
-		prevArrow: '<button type="button" class="slick-arrow arrow-right"><img src="images/icons/arrow-1.svg" alt=""></button>',
-		nextArrow: '<button type="button" class="slick-arrow arrow-left"><img src="images/icons/arrow-2.svg" alt=""></button>',
-	});
-
-
-
+	loop: true,
+	navigation: {
+		nextEl: '.swiper-buttonnext',
+		prevEl: '.swiper-buttonprev',
+	},
 });
+
+
+
